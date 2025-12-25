0828658f916178e197be256d957dd10a0e85371d
Naveen <banjaranaveen89@gmail.com>
2025-12-25 21:56:31 +0530

    Update Hom.jsx file

--- src/Section/Home.jsx
+++ src/Section/Home.jsx
@@ -1,12 +1,23 @@
 import React, { useMemo, useState } from 'react'
-import { motion} from "framer-motion"
+import { motion, scale, spring} from "framer-motion"
 import ParticlesBackground from "../Components/ParticlesBackground"
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import avatar from '../assets/avatar.png'
+import { filter } from 'framer-motion/client'
 
 const socials=[
-  {Icon:FaLinkedin,label:'LinkedIn',link:'https://github.com/naveenbanjara89'},
+  {Icon:FaLinkedin,label:'LinkedIn',link:'https://www.linkedin.com/in/naveen-banjara-88734924b/'},
   {Icon:FaGithub,label:'GitHub',link:'https://github.com/naveenbanjara89'}
 ]
+
+const glowVariants={
+  initial:{scale:1 ,y:0,filter:'drop-shadow(0 0 10px rgba(0,0,0,0) '},
+  hover:{scale:1.2 ,y:-3,
+  filter:'drop-shadow(0 0 8px rgba(13,88,204,0.9)) drop-shadow(0 0 18px rgba(16,185,129,0.9)) ',
+  transition:{type:"spring", stiffness:300, damping:15}
+  },
+  tap:{scale:0.95,y:0,transition:{duration:0.08}}
+}
 
 const Home = () => {
 
@@ -33,7 +44,7 @@ const Home = () => {
   
 
   return (
-    <section id='home' className='w-full h-screen relative bg-black overflow-hidden '>
+    <section id='home' className='w-full h-screen relative bg-black '>
       <ParticlesBackground/>
       <div className='absolute inset-0 '>
         <div className='absolute -top-32 -left-32 w-[70vw] sm:w-[z-500vw] md::w-[40vw] h-[70vw] sm:h-[50vw] md:h-[40vw] max-w-[500px] max-h-[500px] rounded-full bg-gradient-to-r from-[#302b63] via-[#00bf8f] to-[#1cd8d2] opacity-30 sm:opacity-20 md:opacity-10 blur-[100px] sm:blur-[130px] md:blur-[150px] animate-pulse ' ></div>
@@ -71,11 +82,48 @@ const Home = () => {
             animate={{opacity:1,}}
             transition={{duration:0.8,delay:0.8}} >
               <a href='projects' className='px-6 py-3 font-medium text-lg bg-gradient-to-r  from-[#302b63] via-[#00bf8f] to-[#1cd8d2] rounded-full shadow-lg hover:scale-105 transition-all  '>View My Work </a>
-              <a id='resume' href='/NaveenBanjaraResume.pdf'  className='px-6 py-3 rounded-full text-lg font-medium bg-white text-black hover:bg-gray-200 shadow-lg hover:scale-105 transition-all  '>My Resume </a>
+              <a id='resume' href='/NaveenBanjaraResume.pdf' download className='px-6 py-3 rounded-full text-lg font-medium bg-white text-black hover:bg-gray-200 shadow-lg hover:scale-105 transition-all  '>My Resume </a>
+              
             </motion.div>
           </div>
+          <div className='mt-10 flex gap-5 md:text-3xl justify-center lg:justify-start gap-6 '>
+              {socials.map(({Icon,label,href})=>(
+                <motion.a className=' text-gray-300 hover:scale-105' 
+                  href={href}
+                  key={label}
+                  target='_blank'
+                  aria-label={label}
+                  rel='noopener noreferrer'
+                  variants={glowVariants}
+                  initial='initial'
+                  whileHover='hover'
+                  whileTap='tap'>
+                  <Icon/>
+                </motion.a>
+              ))}
+            </div>
         </div>
+        <div className="relative hidden lg:block">
+          <div className='absolute top-1/2 -translate-y-1/2 pointer-events-none' 
+          style={{
+            right:'10px',width:"min(22vw,410px)",height:"min(40vw,760px)", borderRadius:"50%",
+            filter:"blur(38px)",opacity:0.32,
+            background:"conic-gradient(from 0deg ,#1cd8d2 ,#00bf8f , #302b63 ,#1cd8d2 ) "
+
+          }}
+          />
+        <motion.img src={avatar} alt="Naveen Banjara"
+        className="absolute top-1/2 -translate-y-1/2 object-contain cursor-pointer select-none pointer-events-none "
+        style={
+          {right:"-30px",width:"min(45vw,780px)",maxHeight:"90vh "}
+        }
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6 }}
+        />
       </div>
+      </div>
+      
 
     </section>
   )
