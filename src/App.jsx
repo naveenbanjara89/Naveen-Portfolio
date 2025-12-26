7568a15410e405adee5e609dada49ce4170feac8
Naveen <banjaranaveen89@gmail.com>
2025-12-26 19:59:21 +0530

    Update IntroAnimation.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './Components/NavBar'
-// import ParticlesBackground from './Components/ParticlesBackground'
+// import ParticlesBackground from './Components/ParticlesBackground' 
 import CustomCursor from './Components/CustomCursor'
 import Home from './Section/Home'
 import About from './Section/About'
@@ -10,9 +10,17 @@ import Footer  from './Section/Footer'
 import Projects from './Section/Projects'
 import Testimonials from './Section/Testimonials'
 import Skills from './Section/Skills'
+import IntroAnimation from './Components/IntroAnimation'
 
 const App = () => {
+
+  const [introDone, setIntroDone] = React.useState(false)
+
   return (
+    <>
+    {!introDone && <IntroAnimation onFinish={()=>setIntroDone(true)} /> }
+
+    {introDone && (
     <div className='relative gradient text-white '>
       <CustomCursor/>
       {/* <ParticlesBackground/> */}
@@ -27,6 +35,8 @@ const App = () => {
       <Footer/> 
 
     </div>
+    )}
+    </>
   )
 }
 
